a106f56b14a7eacc194ba65ebd196bb260ecc77b
kristaps200303 <71753021+kristaps200303@users.noreply.github.com>
2023-03-20 01:49:08 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,42 +1,46 @@
-# python3
-
-
-def build_heap(data):
-    swaps = []
-    # TODO: Creat heap and heap sort
-    # try to achieve  O(n) and not O(n2)
-
-
-    return swaps
-
-
-def main():
-    
-    # TODO : add input and corresponding checks
-    # add another input for I or F 
-    # first two tests are from keyboard, third test is from a file
-
-
-    # input from keyboard
-    n = int(input())
-    data = list(map(int, input().split()))
-
-    # checks if lenght of data is the same as the said lenght
-    assert len(data) == n
-
-    # calls function to assess the data 
-    # and give back all swaps
-    swaps = build_heap(data)
-
-    # TODO: output how many swaps were made, 
-    # this number should be less than 4n (less than 4*len(data))
-
-
-    # output all swaps
-    print(len(swaps))
-    for i, j in swaps:
-        print(i, j)
-
-
-if __name__ == "__main__":
-    main()
+#KO
+import os #KO
+ #KO
+def build_heap(data): #KO
+    swaps = [] #KO
+    n = len(data) #KO
+    for i in range(n // 2, -1, -1): #KO
+        godown(data, i, swaps) #KO
+    return swaps #KO
+ #KO
+def godown(data, i, swaps): #KO
+    n = len(data) #KO
+    min_inde = i #KO
+    lhs = 2 * i + 1 #KO
+    rhs = 2 * i + 2 #KO
+    if lhs < n and data[lhs] < data[min_inde]: #KO
+        min_inde = lhs #KO
+    if rhs < n and data[rhs] < data[min_inde]: #KO
+        min_inde = rhs #KO
+    if min_inde != i: #KO
+        swaps.append((i, min_inde)) #KO
+        data[i], data[min_inde] = data[min_inde], data[i] #KO
+        godown(data, min_inde, swaps) #KO
+ #KO
+def main(): #KO
+    userinput = input().strip() #KO
+    if "I" in userinput: #KO
+        n = int(input().strip()) #KO
+        data = list(map(int, input().strip().split())) #KO
+        assert len(data) == n #KO
+    elif "F" in userinput: #KO
+        f = input().strip() #KO
+        loc = './tests/' #KO
+        floc = os.path.join(loc, f) #KO
+        with open(floc, mode="r") as file: #KO
+            n = int(file.readline().strip()) #KO
+            data = list(map(int, file.readline().strip().split()) #KO
+        assert len(data) == n #KO
+    swaps = build_heap(data) #KO
+    print(len(swaps)) #KO
+    for i, j in swaps: #KO
+        print(i, j) #KO
+#KO
+if __name__ == "__main__": #KO
+    main() #KO
+#KO
