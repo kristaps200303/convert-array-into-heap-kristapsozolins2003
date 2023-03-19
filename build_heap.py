ee5f5084308e4fbe2eb11af0c5f84f34454c1490
kristaps200303 <71753021+kristaps200303@users.noreply.github.com>
2023-03-20 01:39:49 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,42 +1,45 @@
-# python3
-
+import os
 
 def build_heap(data):
     swaps = []
-    # TODO: Creat heap and heap sort
-    # try to achieve  O(n) and not O(n2)
-
-
+    n = len(data)
+    for i in range(n // 2, -1, -1):
+        godown(data, i, swaps)
     return swaps
 
-
+def godown(data, i, swaps):
+    n = len(data)
+    min_inde = i
+    lhs = 2 * i + 1
+    rhs = 2 * i + 2
+    if lhs < n and data[lhs] < data[min_inde]:
+        min_inde = lhs
+    if rhs < n and data[rhs] < data[min_inde]:
+        min_inde = rhs
+    if min_inde != i:
+        swaps.append((i, min_inde))
+        data[i], data[min_inde] = data[min_inde], data[i]
+        godown(data, min_inde, swaps)
+        
 def main():
-    
-    # TODO : add input and corresponding checks
-    # add another input for I or F 
-    # first two tests are from keyboard, third test is from a file
-
-
-    # input from keyboard
-    n = int(input())
-    data = list(map(int, input().split()))
-
-    # checks if lenght of data is the same as the said lenght
-    assert len(data) == n
-
-    # calls function to assess the data 
-    # and give back all swaps
-    swaps = build_heap(data)
-
-    # TODO: output how many swaps were made, 
-    # this number should be less than 4n (less than 4*len(data))
-
-
-    # output all swaps
+    userinput = input()
+    if "I" in userinput:
+        n = int(input())
+        data = list(map(int, input().split()))
+        assert len(data) == n
+    elif "F" in userinput:
+        f = input()
+        loc = './tests/'
+        floc = os.path.join(loc, f)
+        with open(floc, mode="r") as file:
+            n = int(file.readline())
+            data = list(map(int, file.readline().split()))
+   swaps = build_heap(data)
     print(len(swaps))
     for i, j in swaps:
         print(i, j)
-
-
+        
 if __name__ == "__main__":
     main()
+    
+#KristapsOzolins
